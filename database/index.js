8d4387d8cfc3156d1759008e9ca2fddeb0ce8763
MozartSoares <mozartarmstrong@gmail.com>
2025-03-18 17:44:01 -0300

    fix: ssl error

--- database/index.js
+++ database/index.js
@@ -19,22 +19,12 @@ if (process.env.NODE_ENV == "development") {
       rejectUnauthorized: false,
     },
   });
-
-  // Added for troubleshooting queries
-  // during development
-  query = async (text, params) => {
-    try {
-      const res = await pool.query(text, params);
-      console.log("executed query", { text });
-      return res;
-    } catch (error) {
-      console.error("error in query", { text });
-      throw error;
-    }
-  };
 } else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
 }
 
